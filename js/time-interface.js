dd643df37209bcdb5eb5f8d5ee532cbefec78209
Jason Wuerch and Lindsey <student@epicodus.com>
2016-03-15 13:18:07 -0700

    finalize clock functionality. FIxed alarm clock with a wrench and a screwdriver.

--- js/time-interface.js
+++ js/time-interface.js
@@ -1,17 +1,20 @@
 var AlarmTime = require('./../js/alarm.js').AlarmModule;
 
 $(document).ready(function() {
-
+  setInterval(function() {
+    var time = moment().format('HH:mm');
+    $('#time').html(time);
+  }, 1000);
 
   $('.enterTime').submit(function(event){
     event.preventDefault();
 
     setInterval(function() {
       var time = moment().format('HH:mm');
-      $('#time').html(time);
-    }, 60000);
+    }, 1000);
     var firstName = $('#firstName').val();
     var alarmTime = $('#alarm-time').val();
+    console.log( alarmTime);
     var newAlarm = new AlarmTime(firstName, alarmTime);
     $('.display-area').append(newAlarm.firstName + "<p></p>");
     $('.display-area').append(newAlarm.alarmTime);
